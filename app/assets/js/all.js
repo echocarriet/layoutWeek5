95963ef803a0b1aae74a852748648e9cfd3e6110
echocarrie <echocarriet@gmail.com>
2020-11-23 21:03:21 +0800

    建立 v1

---
+++ app/assets/js/all.js
@@ -0,0 +1,19 @@
+// console.log('Hello!');
+
+// $(document).ready(() => {
+//   console.log('HesSchool Hello!');
+// });
+
+$(document).ready(function () {
+  $('.assignmentCard--Toggle').click(function (e) { 
+    e.preventDefault();
+    $('.assignmentCard-read').toggleClass('active');
+    $('.assignmentCard-reply').toggleClass('active');
+  });
+  // $('assignmentCard-reply--Toggle').click(function (e) { 
+  //   e.preventDefault();
+    
+  // });
+});
+
+
