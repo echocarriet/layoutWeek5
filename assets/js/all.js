8094d5d3b1bf19ff87e0c81cb95254423feeb621
echocarrie <echocarriet@gmail.com>
2020-11-23 21:05:26 +0800

    Update 2020-11-23T13:05:21.791Z

--- assets/js/all.js
+++ assets/js/all.js
@@ -1,19 +1,16 @@
-// console.log('Hello!');
+"use strict";
 
+// console.log('Hello!');
 // $(document).ready(() => {
 //   console.log('HesSchool Hello!');
 // });
-
 $(document).ready(function () {
-  $('.assignmentCard--Toggle').click(function (e) { 
+  $('.assignmentCard--Toggle').click(function (e) {
     e.preventDefault();
     $('.assignmentCard-read').toggleClass('active');
     $('.assignmentCard-reply').toggleClass('active');
-  });
-  // $('assignmentCard-reply--Toggle').click(function (e) { 
+  }); // $('assignmentCard-reply--Toggle').click(function (e) { 
   //   e.preventDefault();
-    
   // });
 });
-
-
+//# sourceMappingURL=all.js.map
